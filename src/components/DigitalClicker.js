0d1b9d2aff571d828b27bbe453563bea4ad41d4a
chrisb3546 <chrisb3546@yahoo.com>
2020-09-23 15:18:10 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,26 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+export default class DigitalClicker extends React.Component{
+
+    state ={
+        timesClicked: 0
+    }
+
+    incrementButton = () => {
+        let newCount = this.state.timesClicked + 1
+        this.setState({
+            timesClicked: newCount
+        })
+
+
+
+    }
+
+    render(){
+        return(
+            <button onClick = {this.incrementButton}>{this.state.timesClicked}</button>
+        )
+
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,47 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+export default class DigitalClicker extends React.Component{
+
+    state = {
+        errors: [],
+        user: null,
+        settings: {
+          bitrate: 8,
+          video: {
+            resolution: '1080p'
+          }
+        }
+      }
+
+      changeToTwelve = () => {
+          let newBitrate = 12
+          this.setState({
+            settings: Object.assign({}, this.state.settings, {
+                bitrate: newBitrate
+              })
+        })
+
+      }
+
+      changeVidQual = () => {
+          let newVidQual = '720p'
+          this.setState({
+              settings: Object.assign({}, this.state.settings,{
+                  video:{
+                resolution: newVidQual
+            }
+              })
+          })
+
+      }
+
+    render(){
+        return(
+            <div>
+        <button className="bitrate" onClick={this.changeToTwelve}>Bitrate Change</button >
+        <button className="resolution" onClick={this.changeVidQual}>Quality Change</button>
+        </div>
+        )
+    }
+}
